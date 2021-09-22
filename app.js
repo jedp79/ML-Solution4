fdaf140302a1ec602fd7cc51b1e62962ab521c19
jedp79 <89169203+jedp79@users.noreply.github.com>
2021-09-22 16:26:41 -0300

    Update app.js

--- app.js
+++ app.js
@@ -2,13 +2,6 @@ const express = require('express');
 const app = express();
 app.use(express.static('public'));
 
-
-//app.listen(3000, ()=>{
-//    console.log('Servidor funcionando');
-//});
-
-//Configuración del puerto - HEROKU
-
 app.listen(process.env.PORT || 3000, function() {
 console.log('Servidor corriendo en el pueerto 3000');
 })
