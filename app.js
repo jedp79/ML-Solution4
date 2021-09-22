aaeb39da99ae34fcf3853ded9d1f710294de3ae8
Jose Perrier <89169203+jedp79@users.noreply.github.com>
2021-09-22 16:27:57 +0000

    Configuración del puerto - HEROKU

--- app.js
+++ app.js
@@ -3,13 +3,22 @@ const app = express();
 app.use(express.static('public'));
 
 
-app.listen(3000, ()=>{
-    console.log('Servidor funcionando');
-});
+//app.listen(3000, ()=>{
+//    console.log('Servidor funcionando');
+//});
+
+//Configuración del puerto - HEROKU
+
+app.listen(process.env.PORT || 3000, function() {
+console.log('Servidor corriendo en el pueerto 3000');
+})
 
 app.get('/', (req,res)=>{
     res.sendFile(__dirname + '/views/home.html');
 });
 app.get('/register', (req,res)=>{
     res.sendFile(__dirname + '/views/register.html');
+});
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
 });
